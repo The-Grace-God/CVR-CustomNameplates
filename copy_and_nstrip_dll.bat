daf0724d53d867ef3acac9ca8f68bf948c20c527
SMUFED <66783188+The-Grace-God@users.noreply.github.com>
2024-10-18 18:40:44 -0500

    oop broken path

--- copy_and_nstrip_dll.bat
+++ copy_and_nstrip_dll.bat
@@ -2,13 +2,13 @@
 setlocal enableDelayedExpansion
 
 echo Copy all dll from the game so we can work and modify them
-xcopy /f /y "%CVRPATH%\MelonLoader\0Harmony.dll" "%0\..\ManagedLibs"
-xcopy /f /y "%CVRPATH%\MelonLoader\MelonLoader.dll" "%0\..\ManagedLibs"
-xcopy /f /s /y "%CVRPATH%\ChilloutVR_Data\Managed" "%0\..\ManagedLibs"
+xcopy /f /y "C:\Program Files (x86)\Steam\steamapps\common\ChilloutVR\MelonLoader\0Harmony.dll" "%0\..\ManagedLibs"
+xcopy /f /y "C:\Program Files (x86)\Steam\steamapps\common\ChilloutVR\MelonLoader\MelonLoader.dll" "%0\..\ManagedLibs"
+xcopy /f /s /y "C:\Program Files (x86)\Steam\steamapps\common\ChilloutVR\ChilloutVR_Data\Managed" "%0\..\ManagedLibs"
 
 echo Nstrip convert all private/protected stuff to public, yay
 for %%x in (Assembly-CSharp.dll Assembly-CSharp-firstpass.dll UnityEngine.CoreModule.dll Cohtml.Runtime.dll) do (
-    NStrip.exe -p -n "%CVRPATH%\ChilloutVR_Data\Managed\%%x" "%0\..\ManagedLibs\%%x"
+    NStrip.exe -p -n "C:\Program Files (x86)\Steam\steamapps\common\ChilloutVR\ChilloutVR_Data\Managed\%%x" "%0\..\ManagedLibs\%%x"
 )
 echo We re done now
 pause
